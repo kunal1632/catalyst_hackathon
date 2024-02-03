3e5ea769cdade6f7ee578b0a6d5566091199fc2e
Ayush Munot <ayushjain1953@gmail.com>
2024-02-03 13:20:53 +0530

    File Structure Changed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Contact } from './pages/Contact';
-import { About } from './pages/About';
+import { App } from './App';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about/",
-    element: <About/>,
-  },
-  {
-    path: "/contact/",
-    element: <Contact/>,
-  },
-  {
-    path: "/profile",
-    element: <Profile/>,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
