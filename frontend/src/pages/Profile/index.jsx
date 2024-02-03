eb9257142e28570598ee42c324233bfd27a5fa5a
Ayush Munot <ayushjain1953@gmail.com>
2024-02-03 17:08:20 +0530

    profile tabs and post

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { PostList } from '../../components/PostList'
+import { ProfileTabs } from '../../components/ProfileTabs'
 
 export const Profile = () => {
   return (
-    <div>Profile</div>
+    <div>
+      <ProfileTabs />
+      <PostList />
+    </div>
   )
 }
