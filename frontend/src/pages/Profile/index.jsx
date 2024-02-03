08a58b960578e346231d0a57b02f54f34abb3a52
imlakshay08 <tyagilakshay119@gmail.com>
2024-02-03 17:28:29 +0530

    Update index.jsx of Profile

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import About from '../../components/About'
+import YouMightKnow from '../../components/YouMightKnow'
+import Active from '../../components/Active'
 
 export const Profile = () => {
   return (
-    <div>Profile</div>
+    <div>
+      <About></About>
+      <YouMightKnow></YouMightKnow>
+      <Active></Active>
+    </div>
   )
 }
